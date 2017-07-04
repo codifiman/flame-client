f7290520956119535080c2d96d34fd46c731f6f5
codifiman <codi.fiman@gmail.com>
2017-07-03 18:24:32 -0700

    Add sliding functionality to arm-switch and disable scroll bouncing on desktop (still happens in iOS Safari...)

--- src/app/arm-switch/arm-switch.component.ts
+++ src/app/arm-switch/arm-switch.component.ts
@@ -19,10 +19,13 @@ export class ArmSwitchComponent {
   @Input() lock: LockState;
   @Output() onEvent = new EventEmitter<Action>();
 
+  arming = false;
+
   get buttonClasses() {
     return {
       armed: this.lock === LOCKED,
-      ready: this.lock === UNLOCKED,
+      arming: this.arming,
+      ready: this.lock === UNLOCKED && !this.arming,
       lockedout: this.lock === LOCKED_OUT
     };
   }
@@ -30,20 +33,76 @@ export class ArmSwitchComponent {
   get buttonText(): string {
     const phrases = {
       [LOCKED]: 'Armed',
-      [UNLOCKED]: 'Inactive',
+      [UNLOCKED]: 'Slide to arm',
       [LOCKED_OUT]: 'Busy',
     };
 
     return phrases[this.lock] || 'Hello!';
   }
 
-  private toggleButton(): void {
-    if (this.lock === LOCKED_OUT) return
+  startDrag(e): void {
+    e.preventDefault();
+    e.stopPropagation();
 
-    if (this.lock === LOCKED) {
-      this.onEvent.emit(DISARM);
-    } else {
-      this.onEvent.emit(ARM);
+    if (this.lock !== LOCKED_OUT) {
+      const slider = e.target;
+      const slide = e.target.parentElement;
+      let targetTouches = e.targetTouches;
+
+      this.arming = true;
+
+      const slideRect = slide.getBoundingClientRect();
+      const sliderRect = slider.getBoundingClientRect();
+      const maxX = slideRect.width - sliderRect.width;
+      const threshold = maxX * 0.75;
+
+      slider.style.left = this.lock === LOCKED ? maxX : '';
+
+      const moveSlider = (e) => {
+        targetTouches = e.targetTouches;
+        const pointerPos = (e.clientX || targetTouches.item(0).clientX) - slideRect.left;
+
+        let x = pointerPos - (sliderRect.width / 2);
+        x = Math.max(0, x);
+        x = Math.min(maxX, x);
+
+        slider.style.left = x + 'px';
+      };
+
+      const stopDrag = (e) => {
+        slider.removeEventListener('mousemove', moveSlider);
+        slider.removeEventListener('touchmove', moveSlider);
+        slider.removeEventListener('mouseleave', stopDrag);
+        slider.removeEventListener('touchleave', stopDrag);
+        slider.removeEventListener('mouseup', stopDrag);
+        slider.removeEventListener('touchend', stopDrag);
+
+        this.arming = false;
+        const pointerPos = (e.clientX || targetTouches.item(0).clientX) - slideRect.left;
+
+        if (pointerPos < maxX) {
+          slider.style.transition = 'left 0.5s';
+        }
+
+        if (pointerPos >= threshold) {
+          this.onEvent.emit(ARM);
+          slider.style.left = `${maxX}px`;
+        } else {
+          slider.style.left = '0px';
+          if (this.lock === LOCKED) {
+            this.onEvent.emit(DISARM);
+          }
+        }
+
+        setTimeout(() => slider.style.transition = '', 400);
+      };
+
+      slider.addEventListener('mousemove', moveSlider);
+      slider.addEventListener('touchmove', moveSlider);
+      slider.addEventListener('mouseleave', stopDrag);
+      slider.addEventListener('touchleave', stopDrag);
+      slider.addEventListener('mouseup', stopDrag);
+      slider.addEventListener('touchend', stopDrag);
     }
   }
 }
